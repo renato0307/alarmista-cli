30d809be63a88e271bb16455c1e80d5ab08aa39c
Renato Torres <renato.torres@unit-consulting.ae>
2019-03-23 08:13:38 +0000

    Fix script to send code to raspberry

--- scripts/send-to-rasp.bat
+++ scripts/send-to-rasp.bat
@@ -1,2 +1,2 @@
 @echo off
-scp ..\cmd\alarmista-cli\*.go pi@rasp2esp32.ddns.net:/home/pi/go/src/github.com/renato0307/alarmista-cli/cmd/alarmista-cli
+scp cmd\alarmista-cli\*.go pi@rasp2esp32.ddns.net:/home/pi/go/src/github.com/renato0307/alarmista-cli/cmd/alarmista-cli
