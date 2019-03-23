a86a87d6d95be43395c6d6d79473651eca709ca8
Renato Torres <renato.torres@unit-consulting.ae>
2019-03-23 07:44:45 +0000

    Formatting applied

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	"os"
-	"gobot.io/x/gobot/platforms/ble"
+
 	"gobot.io/x/gobot/drivers/gpio"
-	"gobot.io/x/gobot/platforms/raspi"		
+	"gobot.io/x/gobot/platforms/ble"
+	"gobot.io/x/gobot/platforms/raspi"
 )
 
 // readCharacteristic reads the value of a characteristic
@@ -31,40 +32,40 @@ func writeCharacteristic(bleAdaptor *ble.ClientAdaptor, uuid string, data []byte
 
 func main() {
 
-	// read characteristic BLE command 
+	// read characteristic BLE command
 	readCharacteristicCmd := flag.NewFlagSet("readc", flag.ExitOnError)
 	readCAddress := readCharacteristicCmd.String("address", "", "the address of the BLE device, e.g. 30:AE:A4:02:BC:3A (required)")
 	readCUuid := readCharacteristicCmd.String("uuid", "", "the UUID of the characteristic, e.g. cc0bd427-c9c3-43b0-a7c6-2df108b2b7c4 (required)")
 
-	// write characteristic BLE command 
+	// write characteristic BLE command
 	writeCharacteristicCmd := flag.NewFlagSet("writec", flag.ExitOnError)
 	writeCAddress := writeCharacteristicCmd.String("address", "", "the address of the BLE device, e.g. 30:AE:A4:02:BC:3A (required)")
-	writeCUuid := writeCharacteristicCmd.String("uuid", "", "the UUID of the characteristic, e.g. cc0bd427-c9c3-43b0-a7c6-2df108b2b7c4 (required)")	
-	writeCValue := writeCharacteristicCmd.String("value", "", "the value to write")	
+	writeCUuid := writeCharacteristicCmd.String("uuid", "", "the UUID of the characteristic, e.g. cc0bd427-c9c3-43b0-a7c6-2df108b2b7c4 (required)")
+	writeCValue := writeCharacteristicCmd.String("value", "", "the value to write")
 
-	// write gpio command 
+	// write gpio command
 	gpioWriteCmd := flag.NewFlagSet("gpio", flag.ExitOnError)
-	gpioWritePin := gpioWriteCmd.String("pin", "", "the pin number to write, e.g. 12 (required)")	
-	gpioWriteValue := gpioWriteCmd.Int("value", 1, "1 or 0")	
+	gpioWritePin := gpioWriteCmd.String("pin", "", "the pin number to write, e.g. 12 (required)")
+	gpioWriteValue := gpioWriteCmd.Int("value", 1, "1 or 0")
 
 	if len(os.Args) < 2 {
-        fmt.Println("readc, writec or gpio subcommand is required")
-        os.Exit(1)
+		fmt.Println("readc, writec or gpio subcommand is required")
+		os.Exit(1)
 	}
-	
+
 	// check which subcommand was executed
 	switch os.Args[1] {
-		case "readc":
-			readCharacteristicCmd.Parse(os.Args[2:])
-		case "writec":
-			writeCharacteristicCmd.Parse(os.Args[2:])
-		case "gpio":
-			gpioWriteCmd.Parse(os.Args[2:])
-		default:
-			flag.PrintDefaults()
-			os.Exit(1)
+	case "readc":
+		readCharacteristicCmd.Parse(os.Args[2:])
+	case "writec":
+		writeCharacteristicCmd.Parse(os.Args[2:])
+	case "gpio":
+		gpioWriteCmd.Parse(os.Args[2:])
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-	
+
 	// handles read characteristic
 	if readCharacteristicCmd.Parsed() {
 		if *readCAddress == "" || *readCUuid == "" {
@@ -77,18 +78,18 @@ func main() {
 
 		cerr := bleAdaptor.Connect()
 		if cerr != nil {
-			fmt.Println("connect NOK", cerr);
+			fmt.Println("connect NOK", cerr)
 			return
 		}
 		fmt.Println("connect OK")
-	
+
 		readCharacteristic(bleAdaptor, *readCUuid)
-	
+
 		derr := bleAdaptor.Disconnect()
 		if derr != nil {
-			fmt.Println("disconnect NOK", derr);
+			fmt.Println("disconnect NOK", derr)
 		} else {
-			fmt.Println("disconnect OK");
+			fmt.Println("disconnect OK")
 		}
 	}
 
@@ -104,18 +105,18 @@ func main() {
 
 		cerr := bleAdaptor.Connect()
 		if cerr != nil {
-			fmt.Println("connect NOK", cerr);
+			fmt.Println("connect NOK", cerr)
 			return
 		}
 		fmt.Println("connect OK")
-	
+
 		writeCharacteristic(bleAdaptor, *writeCUuid, []byte(*writeCValue))
-	
+
 		derr := bleAdaptor.Disconnect()
 		if derr != nil {
-			fmt.Println("disconnect NOK", derr);
+			fmt.Println("disconnect NOK", derr)
 		} else {
-			fmt.Println("disconnect OK");
+			fmt.Println("disconnect OK")
 		}
 	}
 
@@ -134,14 +135,14 @@ func main() {
 		if *gpioWriteValue == 1 {
 			gerr := writePin.On()
 			if gerr != nil {
-				fmt.Println("on NOK", gerr);
+				fmt.Println("on NOK", gerr)
 				return
 			}
 			fmt.Println("on OK")
 		} else if *gpioWriteValue == 0 {
 			gerr := writePin.Off()
 			if gerr != nil {
-				fmt.Println("off NOK", gerr);
+				fmt.Println("off NOK", gerr)
 				return
 			}
 			fmt.Println("off OK")
